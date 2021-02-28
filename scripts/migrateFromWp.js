b0f80b66ca2a8cd1b30a7d92badbaa077f615400
Sérgio Júnior <sergioamjr91@gmail.com>
2021-02-28 19:54:06 -0300

    Saving thumbnail

--- scripts/migrateFromWp.js
+++ scripts/migrateFromWp.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 const fs = require("fs");
 
-const BASE_URL = "https://diariodeumcrohnista.com.br/wp-json/wp/v2/posts";
+const BASE_URL =
+  "https://diariodeumcrohnista.com.br/wp-json/wp/v2/posts?_embed";
 const DIST = "src/wp";
 
 function cleanPath() {
@@ -12,6 +13,19 @@ function cleanPath() {
     if (err) throw err;
   });
   console.log(`${DIST} has been deleted and created.`);
+}
+
+async function downloadImage(url, path) {
+  const extension = url.replace(url.substring(0, url.lastIndexOf(".")), "");
+  axios({ url, responseType: "stream" }).then((response) => {
+    return new Promise((resolve, reject) => {
+      response.data
+        .pipe(fs.createWriteStream(`${path}/thubmnail${extension}`))
+        .on("close", resolve)
+        .on("finish", resolve)
+        .on("error", reject);
+    });
+  });
 }
 
 async function getAllPosts() {
@@ -34,8 +48,11 @@ excerpt: ${excerpt.rendered}
 ${content.rendered}
 `;
 
-function writePost(post) {
+async function writePost(post) {
   try {
+    const thumbnail =
+      post?._embedded["wp:featuredmedia"][0]?.media_details?.sizes?.full
+        ?.source_url;
     const path = `${DIST}/${post.slug}`;
     fs.mkdirSync(path, { recursive: true }, (err) => {
       if (err) throw err;
@@ -43,6 +60,9 @@ function writePost(post) {
     fs.writeFileSync(`${path}/index.mdx`, template(post), (err) => {
       if (err) throw err;
     });
+    if (thumbnail) {
+      await downloadImage(thumbnail, path);
+    }
     console.log(`Page ${post.slug} successfully created.`);
   } catch (err) {
     console.error("writePost", err);
